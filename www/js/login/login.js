1ed4357aaee178b9ac7e51b1ccc10a27eda586d7
宛越 <634706275@qq.com>
2016-06-23 22:58:32 +0800

    ok

--- www/js/login/login.js
+++ www/js/login/login.js
@@ -3,12 +3,14 @@ angular.module('starter.logins', [])
 
 .controller('login', function($scope) {
 	$scope.goLogin = function(){
-		// window.location.href = "#/login";
+		$scope.test = "test";
+		window.location.href = "#/main";
 	}
 })
 
-.controller('goLogin', function($scope) {
-	$scope.goLogin = function(){
-		window.location.href = "#/main";
-	}
-});
+// .controller('goLogin', function($scope) {
+// 	$scope.goLogin = function(){
+// 		// $scope.test = "ss";
+// 		window.location.href = "#/main";
+// 	}
+// });
