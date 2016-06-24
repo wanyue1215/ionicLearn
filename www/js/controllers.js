554957d2380362e9f3386303180071252a3cbe50
宛越 <634706275@qq.com>
2016-06-24 11:42:04 +0800

    ok2

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -18,4 +18,21 @@ angular.module('starter.controllers', [])
 .controller('calendar', function($scope) {
 	console.warn("calendar");
 	$scope.title = "test";
+})
+
+// 列表控制器
+.controller('personList',function($scope,$http){
+	$http.get('js/data.json').success(function(data){
+		$scope.lists = data.artists;
+		$scope.moveItem = function(item,formIndex,toIndex){
+			$scope.lists.splice(formIndex,1);
+			$scope.lists.splice(toIndex,0,item);
+		};
+		$scope.onItemDelete = function(item){
+			$scope.lists.splice($scope.lists.indexOf(item),1)
+		};
+	});
 });
+
+
+
